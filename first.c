7f14738077403c4a070371ac9c3f485d6ed44784
soham-raskar <sohamtyu123@gmail.com>
2025-06-09 11:21:55 +0530

    second repo

---
+++ first.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+      printf("Uplaoding from the local machine \n");
+
+      // finding the odd number
+
+      int a;
+
+      printf("Enter a Number\n");
+      scanf("%d", &a);
+
+      if (a % 2 == 0)
+      {
+            printf("Even number");
+      }
+
+      else
+            printf("Odd number ");
+      return 0;
+}
